609c84b929f00d085806883f9cdfe91b2c390b0d
Alisia Michel <cc0-11@outlook.de>
2019-07-03 15:15:10 +0200

    form on the show page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :restaurants, only: [:create, :index, :new, :show] do
-    resources :reviews, only: [:new, :create, :index]
+    resources :reviews, only: [:create]
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
